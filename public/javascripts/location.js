a7e64a721ff99199600acf9c6f9ab91b4b5e5bc6
Nagaraj Bhat <nagarajbhat12@gmail.com>
2016-02-11 01:45:52 +0530

    changed zoom to 12 and marker color to red variation

--- public/javascripts/location.js
+++ public/javascripts/location.js
@@ -16,7 +16,7 @@
 	    var lati =position.coords.latitude;
 	    var longi=position.coords.longitude;
 	    L.mapbox.accessToken = 'pk.eyJ1IjoibmFnYXJhamJoYXQiLCJhIjoiY2lrZnBvc2tsMDAyZHZwbTB3Z3ZuZDg1MiJ9.-fWmFnJhDitJIbPu2qHGUA';
-	var map=L.mapbox.map('map-two', 'mapbox.streets').setView([lati, longi],14);	
+	var map=L.mapbox.map('map-two', 'mapbox.streets').setView([lati, longi],12);	
 
 
 	L.mapbox.featureLayer({
@@ -34,11 +34,11 @@
 	    },
 	    properties: {
 	        title: 'Your location',
-	        description: 'your current location',
+	        description: 'You are here',
 	        // one can customize markers by adding simplestyle properties
 	        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
 	        'marker-size': 'large',
-	        'marker-color': '#BE9A6B',
+	        'marker-color': '#FF4136',
 	        'marker-symbol': 'star'
 	    }
 	}).addTo(map);
