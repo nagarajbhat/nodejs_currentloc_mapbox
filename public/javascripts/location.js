3aa00f67cadbdbce388dd75bd9459ac71d7d8992
Nagaraj Bhat <nagarajbhat12@gmail.com>
2016-02-11 00:57:51 +0530

    first commit

---
+++ public/javascripts/location.js
@@ -0,0 +1,48 @@
+
+        
+
+	var x = document.getElementById("msg");
+
+	function getLocation() {
+	    if (navigator.geolocation) {
+	        navigator.geolocation.getCurrentPosition(showPosition);
+	    } else { 
+	        x.innerHTML = "Geolocation is not supported by this browser.";
+	    }
+	};
+
+	function showPosition(position) {
+	    
+	    var lati =position.coords.latitude;
+	    var longi=position.coords.longitude;
+	    L.mapbox.accessToken = 'pk.eyJ1IjoibmFnYXJhamJoYXQiLCJhIjoiY2lrZnBvc2tsMDAyZHZwbTB3Z3ZuZDg1MiJ9.-fWmFnJhDitJIbPu2qHGUA';
+	var map=L.mapbox.map('map-two', 'mapbox.streets').setView([lati, longi],14);	
+
+
+	L.mapbox.featureLayer({
+	    // this feature is in the GeoJSON format: see geojson.org
+	    // for the full specification
+	    type: 'Feature',
+	    geometry: {
+	        type: 'Point',
+	        // coordinates here are in longitude, latitude order because
+	        // x, y is the standard for GeoJSON and many formats
+	        coordinates: [
+	          longi,
+	          lati 
+	        ]
+	    },
+	    properties: {
+	        title: 'Your location',
+	        description: 'your current location',
+	        // one can customize markers by adding simplestyle properties
+	        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+	        'marker-size': 'large',
+	        'marker-color': '#BE9A6B',
+	        'marker-symbol': 'star'
+	    }
+	}).addTo(map);
+
+	   
+	};
+
